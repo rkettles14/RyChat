841e7176af0ab9d8cae4844f58ab996a43a1772e
rkettles14 <rkettles14@gmail.com>
2020-11-07 18:33:15 -0700

    Separate style

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+
+app.use(express.static("public"));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
+    console.log('a user connected');
     socket.on('chat message', (msg) => {
         io.emit('chat message', msg);
     });
