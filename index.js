c5178c0c16c03feba17f17a9f603dc466c353e95
rkettles14 <rkettles14@gmail.com>
2020-11-07 21:00:27 -0700

    add stuff

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+var counter = 1;
 
 app.use(express.static("public"));
 
@@ -10,11 +11,17 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
+    console.log('Socket id:' + socket.id + ' has connected');
+    io.to(socket.id).send("User" + counter);
+    counter++;
     socket.on('chat message', (msg) => {
         io.emit('chat message', msg);
     });
 });
+
+io.on('reconnect', (socket) => {
+    console.log('Socket id:' + socket.id + ' has reconnected');
+})
 
 http.listen(3000, () => {
     console.log('listening on *:3000');
