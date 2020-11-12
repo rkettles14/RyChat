b67cfa6b8d83274c2311c0c169bc7c68a0ac4245
rkettles14 <rkettles14@gmail.com>
2020-11-12 00:24:25 -0700

    changed everything

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+const { urlencoded } = require('express');
 var express = require('express');
 var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 var fs = require('fs');
-
+const { disconnect } = require('process');
 
 
 app.use(express.static("public"));
@@ -14,25 +15,16 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
     console.log('Socket id:' + socket.id + ' has connected');
+    var user = getRandomUsername();
+    var color = getRandomColor();
 
     //check if new user
     if (!socket.handshake.headers.cookie) {
         io.to(socket.id).emit('send user', {
-            username: getRandomUsername(),
-            color: getRandomColor()
+            username: user,
+            color: color
         });
-    }
-
-    //send cache to client
-    fs.readFile(__dirname + '/cache.json', (err, data) => {
-        if (err) throw err;
-        var json = JSON.parse(data);
-        if (json.length !== 0) {
-            io.to(socket.id).emit('get cache', JSON.parse(data))
-        }
-    });
-
-    socket.on('refresh chat', () => {
+        //send cache to client
         fs.readFile(__dirname + '/cache.json', (err, data) => {
             if (err) throw err;
             var json = JSON.parse(data);
@@ -40,11 +32,110 @@ io.on('connection', (socket) => {
                 io.to(socket.id).emit('get cache', JSON.parse(data))
             }
         });
+
+        //write to user log
+        fs.readFile(__dirname + '/users.json', (err, data) => {
+            if (err) throw err;
+            var json = JSON.parse(data);
+            json.push({ username: user, color: color });
+            var temp = [];
+            json.forEach(element => {
+                temp.push(JSON.stringify(element));
+            })
+            temp = temp.filter((v, i, a) => {
+                return a.indexOf((v)) == i; //remove duplicates
+            });
+            var userArray = [];
+            temp.forEach(element => {
+                userArray.push(JSON.parse(element));
+            })
+            io.emit('get online users', userArray); //send online users
+            json = JSON.stringify(json, null, 2);
+            fs.writeFile(__dirname + '/users.json', json, (err) => {
+                if (err) throw err;
+                console.log('Cache updated');
+            });
+        });
+    } else {
+        //record user in user log
+        fs.readFile(__dirname + '/users.json', (err, data) => {
+            if (err) throw err;
+            var json = JSON.parse(data);
+            json.push({ username: parseUsername(socket.handshake.headers.cookie), color: parseColor(socket.handshake.headers.cookie) });
+            var temp = [];
+            json.forEach(element => {
+                temp.push(JSON.stringify(element));
+            })
+            temp = temp.filter((v, i, a) => {
+                return a.indexOf((v)) == i; //remove duplicates
+            });
+            var userArray = [];
+            temp.forEach(element => {
+                userArray.push(JSON.parse(element));
+            })
+            io.emit('get online users', userArray); //send online users
+            json = JSON.stringify(json, null, 2);
+            fs.writeFile(__dirname + '/users.json', json, (err) => {
+                if (err) throw err;
+                console.log('Cache updated');
+            });
+        });
+    }
+
+    socket.on('disconnect', () => {
+        fs.readFile(__dirname + '/users.json', (err, data) => {
+            if (err) throw err;
+            var json = JSON.parse(data);
+            var name = socket.handshake.headers.cookie ? parseUsername(socket.handshake.headers.cookie) : user; //if this is initial connection or not
+            for (let i = 0; i < json.length; i++) {
+                if (json[i].username == name) {
+                    json.splice(i, 1);
+                    break;
+                }
+            }
+            var temp = [];
+            json.forEach(element => {
+                temp.push(JSON.stringify(element));
+            })
+            temp = temp.filter((v, i, a) => {
+                return a.indexOf((v)) == i; //remove duplicates
+            });
+            var userArray = [];
+            temp.forEach(element => {
+                userArray.push(JSON.parse(element));
+            })
+            io.emit('get online users', userArray); //send online users
+            json = JSON.stringify(json, null, 2);
+            fs.writeFile(__dirname + '/users.json', json, (err) => {
+                if (err) throw err;
+                console.log('Cache updated');
+            });
+        });
+    });
+
+    socket.on
+
+    socket.on('refresh chat', () => {
+        fs.readFile(__dirname + '/cache.json', (err, data) => {
+            if (err) throw err;
+            var json = JSON.parse(data);
+            if (json.length !== 0) {
+                io.emit('get cache', JSON.parse(data))
+            }
+        });
+    })
+
+    socket.on('clear cache', () => {
+        emptyArray = "[]";
+        fs.writeFile(__dirname + '/cache.json', emptyArray, (err) => {
+            if (err) throw err;
+            console.log("cache cleared");
+        });
     })
 
     socket.on('update username', function(credentials, callback) {
         var isSuccess = true;
-        //add message to cache
+
         fs.readFile(__dirname + '/cache.json', function(err, data) {
             if (err) throw err;
 
@@ -57,7 +148,7 @@ io.on('connection', (socket) => {
             });
 
             if (isSuccess) {
-                //update username in log
+                //update username in cache
                 json.forEach(element => {
                     if (element.user === credentials.oldName) {
                         element.user = credentials.newName;
@@ -71,15 +162,15 @@ io.on('connection', (socket) => {
                 });
             }
             callback(isSuccess);
-        });
+        }); //TODO update usernames in log!
     });
 
     socket.on('update color', function(credentials, callback) {
         var isSuccess = true;
-        if (credentials.newColor.length != 9 || !/^\d+$/.test(credentials.newColor)) {
+        if (credentials.newColor.length != 6 || !/^[0-9a-fA-F]+$/.test(credentials.newColor)) {
             isSuccess = false;
         }
-        let newColor = "rgb(" + credentials.newColor.substring(0, 3) + "," + credentials.newColor.substring(3, 6) + "," + credentials.newColor.substring(6) + ")";
+        let newColor = "#" + credentials.newColor;
         fs.readFile(__dirname + '/cache.json', function(err, data) {
             if (err) throw err;
 
@@ -92,7 +183,7 @@ io.on('connection', (socket) => {
             });
 
             if (isSuccess) {
-                //update username in log
+                //update username in cache
                 json.forEach(element => {
                     if (element.color === credentials.oldColor) {
                         element.color = newColor;
@@ -105,10 +196,9 @@ io.on('connection', (socket) => {
                     console.log('Cache updated');
                 });
             }
-            callback(isSuccess, newColor);
+            callback(isSuccess, newColor); //TODO update user color in user log! ALSO TODO: HANDLE DUplicate USERS IN LOG
         });
     });
-
 
     socket.on('chat message', (msg) => {
         //add message to cache
@@ -143,4 +233,12 @@ function getRandomColor() {
 
 function getRandomUsername() {
     return "User" + Math.floor(Math.random() * 999999).toString().padStart(6, '0');
+}
+
+function parseUsername(cookie) {
+    return cookie.split('; ').find(row => row.startsWith('username')).split('=')[1];
+}
+
+function parseColor(cookie) {
+    return cookie.split('; ').find(row => row.startsWith('color')).split('=')[1];
 }
---
+++ public/app.js
@@ -0,0 +1,167 @@
+var socket = io();
+var chat = new Vue({
+    el: "#chat",
+    data: {
+        messages: []
+    },
+    updated: function() {
+        var container = this.$el.querySelector("#container");
+        container.scrollTop = container.scrollHeight;
+    },
+});
+
+var textInput = new Vue({
+    el: "#input",
+    data: {
+        message: "",
+    },
+    methods: {
+        sendMessage: function(e) {
+            e.preventDefault(); // prevents page reloading
+            //check for username change
+            if (this.message.startsWith("/name <") && this.message.endsWith(">")) {
+                //parse new username 
+                var newName = this.message.substring(this.message.indexOf('<') + 1, this.message.lastIndexOf('>'));
+                var oldName = getUsername();
+                socket.emit(
+                    'update username', {
+                        newName: newName,
+                        oldName: oldName
+                    },
+                    function(success) {
+                        if (success) {
+                            document.cookie = "username=" + newName + ";max-age=31536000";
+                            refreshChat();
+                        } else {
+                            alert("That username has already been taken!");
+                        }
+                    }
+                );
+                this.message = "";
+                return false;
+            }
+            //check for color change
+            if (this.message.startsWith("/color ")) {
+                //parse new color 
+                var newColor = this.message.split(" ")[1];
+                var oldColor = getColor();
+                socket.emit(
+                    'update color', {
+                        newColor: newColor,
+                        oldColor: oldColor
+                    },
+                    function(success, color) {
+                        if (success) {
+                            document.cookie = "color=" + color + ";max-age=31536000";
+                            refreshChat();
+                        } else {
+                            alert("That color is invalid or has already been taken!\nPlease specify a color in the following format: \'/color RRGGBB\'");
+                        }
+                    }
+                );
+                this.message = "";
+                return false;
+            }
+            if (this.message === "/clear-cache") {
+                socket.emit('clear cache');
+                refreshChat();
+                this.message = "";
+                return false;
+            }
+
+            socket.emit('chat message', {
+                message: this.message,
+                user: getUsername(),
+                color: getColor(),
+                timeStamp: new Date()
+            });
+            this.message = "";
+            return false;
+        }
+    }
+});
+
+var userDisplay = new Vue({
+    el: "#display",
+    data: {
+        users: []
+    }
+});
+
+function getUsername() {
+    return document.cookie ? document.cookie.split('; ').find(row => row.startsWith('username')).split('=')[1] : undefined;
+}
+
+function getColor() {
+    return color = document.cookie ? document.cookie.split('; ').find(row => row.startsWith('color')).split('=')[1] : undefined;
+}
+
+function refreshChat() {
+    chat.messages = [];
+    $('#name').html(getUsername());
+    $('#name').css("color", getColor());
+    $('.btn').css("background-color", getColor());
+    socket.emit('refresh chat');
+}
+
+socket.on('connect', () => {
+    if (document.cookie) {
+        refreshChat();
+    }
+});
+
+
+socket.on('send user', function(user) {
+    document.cookie = "username=" + user.username + ";max-age=31536000";
+    document.cookie = "color=" + user.color + ";max-age=31536000";
+    $('#name').html(user.username);
+    $('#name').css("color", user.color);
+    $('.btn').css("background-color", user.color);
+});
+
+socket.on('get cache', (cache) => {
+    chat.messages = [];
+    cache.forEach(msg => {
+        var timeStamp = new Date(msg.timeStamp);
+        msg.message = msg.message.replace(":)", String.fromCodePoint(128513));
+        msg.message = msg.message.replace(":(", String.fromCodePoint(128577));
+        msg.message = msg.message.replace(":o", String.fromCodePoint(128562));
+        chat.messages.push({
+            user: msg.user + ":",
+            text: msg.message,
+            timeStamp: timeStamp.getHours() + ':' + (timeStamp.getMinutes() < 10 ? "0" : "") + timeStamp.getMinutes(),
+            style: {
+                color: msg.color,
+                fontWeight: msg.user == getUsername() ? "bold" : "normal"
+            }
+        });
+    });
+});
+
+socket.on('get online users', (users) => {
+    userDisplay.users = [];
+    users.forEach(element => {
+        userDisplay.users.push({
+            name: element.username,
+            style: {
+                color: element.color,
+            }
+        });
+    });
+});
+
+socket.on('chat message', function(msg) {
+    var timeStamp = new Date(msg.timeStamp);
+    msg.message = msg.message.replace(":)", String.fromCodePoint(128513));
+    msg.message = msg.message.replace(":(", String.fromCodePoint(128577));
+    msg.message = msg.message.replace(":o", String.fromCodePoint(128562));
+    chat.messages.push({
+        user: msg.user + ":",
+        text: msg.message,
+        timeStamp: timeStamp.getHours() + ':' + (timeStamp.getMinutes() < 10 ? "0" : "") + timeStamp.getMinutes(),
+        style: {
+            color: msg.color,
+            fontWeight: msg.user == getUsername() ? "bold" : "normal"
+        }
+    });
+});
